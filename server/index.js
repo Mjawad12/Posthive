ad7b22844d192ca8a55834c0d16d648a097cec53
Mjawad12 <143606319+Mjawad12@users.noreply.github.com>
2024-09-06 02:28:29 +0500

    ser

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.get("/check-auth", (req, res) => {
       .status(200)
       .json({ authenticated: true, userId: decoded.userId });
   } catch (error) {
-    return res.status(401).json({ authenticated: false });
+    return res.status(200).json({ authenticated: false });
   }
 });
 
