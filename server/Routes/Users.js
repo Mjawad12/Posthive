893ba9defc7a962768ddaafda8055b9646371eee
Mjawad12 <143606319+Mjawad12@users.noreply.github.com>
2024-09-06 02:43:32 +0500

    correct

--- server/Routes/Users.js
+++ server/Routes/Users.js
@@ -96,7 +96,7 @@ router.post(
       maxAge: 15 * 24 * 60 * 60 * 1000,
       secure: true, // Make this true in production with HTTPS
       sameSite: "lax",
-      domain: "posthive-phi.vercel.app", // Remove protocol (http/https), only domain is required
+      domain: ".vercel.app", // Remove protocol (http/https), only domain is required
       path: "/", // Ensure the cookie is accessible from all paths
     });
     res.status(200).send({ authtoken: Authtoken });
